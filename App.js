6eb019720a14560ee04ae1f154cfea858978796b
San Lin Htike <sanlinhtike@Sans-MacBook-Pro.local>
2022-08-12 23:00:04 +0630

    Text

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View} from "react-native";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <View>
+        <Text style={{ fontSize: 50 }}>Hello App</Text>
+        <Text style={{ fontSize: 20 }}>Babe</Text>
+        <Text style={{ fontSize: 20 }}>I'm Text</Text>
+      </View>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
