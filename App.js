fd6771ac63583812eafaef768d02e4e39a624133
San Lin Htike <sanlinhtike@Sans-MacBook-Pro.local>
2022-08-28 23:04:14 +0630

    Width Height

--- App.js
+++ App.js
@@ -6,8 +6,8 @@ const App = () => {
       <View>
           <SafeAreaView>
               <View>
-                  <Text style={ styles.font_size }>Hello React Native</Text>
-                  <Text>Hello React Native</Text>
+                  <Text style={ styles.font_size }>Width & Height</Text>
+                  <View style={styles.Box}></View>
               </View>
           </SafeAreaView>
       </View>
@@ -19,6 +19,11 @@ const styles = StyleSheet.create({
         fontSize: 30,
         color: "#007bff",
         fontWeight: "bold"
+    },
+    Box: {
+        width: 300,
+        height: 100,
+        backgroundColor: "#007bff"
     }
 })
 
