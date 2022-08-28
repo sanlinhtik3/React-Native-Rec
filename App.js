d5acfbfbfed2ee955804c3c9387287c9e16e0f1c
San Lin Htike <sanlinhtike@Sans-MacBook-Pro.local>
2022-08-28 22:56:16 +0630

    Flex Box

--- App.js
+++ App.js
@@ -1,33 +1,55 @@
-import {TextInput, View, StyleSheet, SafeAreaView} from "react-native";
-import {useState} from "react";
+import {View, StyleSheet, Text} from "react-native";
 
 const App = () => {
-    const [input, setInput] = useState("Ant Man");
 
   return (
-      <View>
-        <SafeAreaView>
-            <TextInput
-                style={styles.input}
-                placeholder="Please Enter Your Name"
-                value={input}
-                onChangeText={setInput}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Please Enter Your Password"
-            />
-        </SafeAreaView>
+      <View style={styles.container}>
+          <Text style={styles.header}>Flex Box</Text>
+
+          <View style={styles.LayoutControl}>
+              <View style={styles.Box}></View>
+              <View style={styles.Box2}></View>
+              <View style={styles.Box3}></View>
+          </View>
+
       </View>
   )
 }
 
 const styles = StyleSheet.create({
-    input: {
-        borderWidth: 1,
-        padding: 10,
-        margin: 12,
-        borderRadius: 5
+    header: {
+        fontSize: 40,
+        fontWeight: "bold",
+        marginTop: 30
+    },
+    container: {
+        flex: 1,
+        backgroundColor: "#ecfdf5"
+    },
+    LayoutControl: {
+        backgroundColor: "#ffe4e6",
+        flex: 1,
+        flexDirection: "row"
+    },
+    Box: {
+        width: 100,
+        height: 100,
+        backgroundColor: "#5eead4",
+        flexGrow: 1,
+        flexShrink: 1,
+        flexBasis: 300,
+    },
+    Box2: {
+        width: 100,
+        height: 100,
+        backgroundColor: "#d8b4fe",
+        flexGrow: 1
+    },
+    Box3: {
+        width: 100,
+        height: 100,
+        backgroundColor: "#a5b4fc",
+        flexGrow: 1
     }
 })
 
